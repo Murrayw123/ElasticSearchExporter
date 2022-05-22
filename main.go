9c2f656fb6d69f4869c0ddec9620a3f3b86dc8d7
murray <murraycwatts@gmail.com>
2022-05-22 20:47:39 +0800

    Added a file based logger

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 )
 
 var client = &http.Client{Timeout: 10 * time.Second}
@@ -21,15 +22,27 @@ var gauge = prometheus.NewGauge(prometheus.GaugeOpts{
 
 var pollFrequency, _ = strconv.Atoi(getEnv("ES_POLL_FREQ", "8"))
 var esUrl = getEnv("ES_URL", "localhost:9200")
+var logPath = "./elastic_search_monitor.log"
 
 func getESData(url string, target interface{}) error {
-	fmt.Println(url)
 	r, err := client.Get(url)
 	if err != nil {
 		return err
 	}
 	defer r.Body.Close()
 	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func setupLogger() *zap.SugaredLogger {
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = []string{logPath, "stdout"}
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	defer logger.Sync()
+	return logger.Sugar()
 }
 
 func getEnv(key, fallback string) string {
@@ -40,7 +53,7 @@ func getEnv(key, fallback string) string {
 	return value
 }
 
-func collectTaskInfo() {
+func collectTaskInfo(logger *zap.SugaredLogger) {
 	taskResp := TaskResp{}
 	url := "http://" + esUrl + "/_tasks?actions=*search&detailed*"
 
@@ -53,23 +66,25 @@ func collectTaskInfo() {
 		for taskKey, _ := range taskResp.Nodes[key].Tasks {
 			task := taskResp.Nodes[key].Tasks[taskKey]
 			gauge.Set(float64(task.RunningTimeInNanos / 1000000))
-			fmt.Printf(" \ntimestamp: %s, task_id: %d, running_time_ms: %d, "+
+			message := fmt.Sprintf("timestamp: %s, task_id: %d, running_time_ms: %d, "+
 				"action: %s, description: %s", time.Now().Format("2006-01-02 15:04:05"), task.Id,
 				task.RunningTimeInNanos/1000000, task.Action, task.Description)
+			logger.Info(message)
 		}
 	}
 }
 
-func collectEvery() {
+func collectEvery(logger *zap.SugaredLogger) {
 	tickEvery := time.Duration(pollFrequency) * time.Second
 
 	for _ = range time.Tick(tickEvery) {
-		collectTaskInfo()
+		collectTaskInfo(logger)
 	}
 }
 
 func main() {
-	go collectEvery()
+	logger := setupLogger()
+	go collectEvery(logger)
 	fmt.Println("Listening")
 	http.Handle("/metrics", promhttp.Handler())
 	prometheus.MustRegister(gauge)
